3aaa266ffa9935a15e7ca8964bee895411b33a94
diegof856 <diegofelipe1025@gmail.com>
2024-05-20 12:15:54 -0300

    adicionando comentarios e uma pequena correcao no codigo

--- src/main/java/com/unifacol/atividade_7/controller/HomeController.java
+++ src/main/java/com/unifacol/atividade_7/controller/HomeController.java
@@ -10,96 +10,89 @@ import org.springframework.web.servlet.ModelAndView;
 import com.unifacol.atividade_7.model.User;
 import com.unifacol.atividade_7.service.UserService;
 
-
 @Controller
 public class HomeController {
 	@Autowired
 	private UserService userService;
-	
-	
-	private static Long idUser =4L ;
-	private User user;
-	
+
+	private static Long idUser; // ID do usuário atual
+	private User user; // Aux
+
+	// Mapeia a URL raiz ("/") para exibir a página inicial
 	@GetMapping("/")
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("home/index");
 		return mv;
 	}
+
+	// Adiciona um novo usuário e exibe uma mensagem de sucesso
 	@PostMapping("/addUser")
-	public ModelAndView addUser(@RequestParam("name") String name, @RequestParam("email") String email ) {
+	public ModelAndView addUser(@RequestParam("name") String name, @RequestParam("email") String email) {
 		this.user = new User(null, name, email);
-		/*userService.addUser(user);
-		idUser = userService.searchByName(name);*/
-		
-		
+		userService.addUser(user);
+		idUser = userService.searchByName(name);
+
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("home/index");
-		mv.addObject("message","Usuario adicionado com sucesso");
+		mv.addObject("message", "Usuario adicionado com sucesso");
 		mv.addObject("nomeUsuario", user.getName());
-		mv.addObject("to_alter","Deseja alterar o nome do usuario?");
-		
-		
-		
-		
+		mv.addObject("to_alter", "Deseja alterar o nome do usuario?");
+
 		return mv;
-	}
-	@PostMapping("/userChoice")
-	public ModelAndView choiceUser(@RequestParam("choice") String response) {
-	    ModelAndView mv = new ModelAndView();
-	    this.user = this.userService.findById(idUser);
-	    
-	    if(response.equals("Sim")) {
-	        mv.setViewName("/home/to_alter");
-	        return mv;
-	    } else {
-	    	 mv.setViewName("/home/index");
-	    	mv.addObject("message","Usuario adicionado com sucesso");
-	    	
-	    	mv.addObject("nomeUsuario", user.getName());
-	    		mv.addObject("to_alter","Deseja alterar o nome do usuario?");
-	    		mv.addObject("delete","Deseja deletar este cadastro");
-	    	 
-	    	 
-	    	 
-	    }
-	    
-	    return mv;
-	}
-	
-	
-	@PostMapping("/userNew")
-	public ModelAndView update(@RequestParam("newName")String novoNome, @RequestParam("newEmail") String novoEmail) {
-		ModelAndView mv = new ModelAndView();
-	this.user = userService.updateById(idUser, novoNome, novoEmail);
-		
-		mv.addObject("message","Novo nome: "+this.user.getName() +"\nNovo email: "+ this.user.getEmail());
-		mv.addObject("message_2","Nome e Email alterados com sucesso");
-		mv.addObject("delete","Deseja deletar este cadastro");
-		mv.setViewName("home/to_alter");
-		return mv;
-		
-	}
-	@PostMapping("/userChoice_Del")
-	public ModelAndView delete(@RequestParam("choice_del")String reponse_del) {
-		ModelAndView mv = new ModelAndView();
-		if(reponse_del.equals("Sim")) {
-			userService.deleteById(idUser);
-			 mv.setViewName("/home/index");
-			mv.addObject("to_alter","Deseja alterar o nome do usuario?");
-			mv.addObject("msg_delete","Usuario deletado com sucesso");
-			return mv;
-		}else {
-			 mv.setViewName("/home/index");
-			mv.addObject("nomeUsuario", user.getName());
-			mv.addObject("to_alter","Deseja alterar o nome do usuario?");
-			mv.addObject("delete","Deseja deletar este cadastro");
-			return mv;
-		}
-		
-		
-		
 	}
 
-	
+	// Recebe a escolha do usuário para alterar ou não os dados
+	@PostMapping("/userChoice")
+	public ModelAndView choiceUser(@RequestParam("choice") String response) {
+		ModelAndView mv = new ModelAndView();
+		this.user = this.userService.findById(idUser);
+
+		if (response.equals("Sim")) {
+			mv.setViewName("/home/to_alter");
+			return mv;
+		} else {
+			mv.setViewName("/home/index");
+			mv.addObject("message", "Usuario adicionado com sucesso");
+			mv.addObject("nomeUsuario", user.getName());
+			mv.addObject("to_alter", "Deseja alterar o nome do usuario?");
+			mv.addObject("delete", "Deseja deletar este cadastro");
+
+		}
+
+		return mv;
+	}
+
+	// Atualiza os dados do usuário e exibe uma mensagem de sucesso
+	@PostMapping("/userNew")
+	public ModelAndView update(@RequestParam("newName") String novoNome, @RequestParam("newEmail") String novoEmail) {
+		ModelAndView mv = new ModelAndView();
+		this.user = userService.updateById(idUser, novoNome, novoEmail);
+
+		mv.addObject("message", "Novo nome: " + this.user.getName() + "\nNovo email: " + this.user.getEmail());
+		mv.addObject("message_2", "Nome e Email alterados com sucesso");
+		mv.addObject("delete", "Deseja deletar este cadastro");
+		mv.setViewName("home/to_alter");
+		return mv;
+	}
+
+	// Recebe a escolha do usuário para deletar ou não o cadastro e executa a ação
+	// correspondente
+	@PostMapping("/userChoice_Del")
+	public ModelAndView delete(@RequestParam("choice_del") String response_del) {
+		ModelAndView mv = new ModelAndView();
+		if (response_del.equals("Sim")) {
+			userService.deleteById(idUser);
+			mv.setViewName("/home/index");
+			mv.addObject("to_alter", "Deseja alterar o nome do usuario?");
+			mv.addObject("msg_delete", "Usuario deletado com sucesso");
+			return mv;
+		} else {
+			mv.setViewName("/home/index");
+			mv.addObject("nomeUsuario", user.getName());
+			mv.addObject("to_alter", "Deseja alterar o nome do usuario?");
+			mv.addObject("delete", "Deseja deletar este cadastro");
+			return mv;
+		}
+	}
 }
--- src/main/java/com/unifacol/atividade_7/service/UserService.java
+++ src/main/java/com/unifacol/atividade_7/service/UserService.java
@@ -10,29 +10,30 @@ import com.unifacol.atividade_7.repositories.UserRepository;
 public class UserService {
 	@Autowired
 	private UserRepository repository;
-	
-	public void addUser(User user) {
-		repository.save(user);
+
+	public void addUser(User user) { //Adiciona um novo usuário ao repositório.
+		repository.save(user); 
 	}
-	
+
 	public Long searchByName(String name) {
-		User user = repository.findByName(name);
+		User user = repository.findByName(name); //Busca um usuário pelo nome e retorna seu ID.
 		return user.getId();
 	}
-	
+
 	public User findById(Long id) {
-		User user  = repository.getReferenceById(id) ;
+		User user = repository.getReferenceById(id); //Busca um usuário pelo ID.
 		return user;
 	}
-	
-	public User updateById(Long id, String name, String email) {
-		
-		 User user = repository.getReferenceById(id);
-		 user.setName(name);
-		 user.setEmail(email);
-		 return repository.save(user);
+
+	public User updateById(Long id, String name, String email) { //Atualiza o nome e email de um usuário pelo ID.
+
+		User user = repository.getReferenceById(id);
+		user.setName(name);
+		user.setEmail(email);
+		return repository.save(user);
 	}
-	public void deleteById(Long id) {
+
+	public void deleteById(Long id) { //Deleta um usuário pelo ID.
 		repository.deleteById(id);
 	}
 
